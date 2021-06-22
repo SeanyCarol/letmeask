ac71348fb24a2bd52b23a1fe5222fc85a1eac77d
SeanyCarol <seany@ice.ufjf.br>
2021-06-22 20:54:56 -0300

    Authentication context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import { useHistory } from 'react-router-dom';
 
-import { firebase, auth } from '../services/firebase';
-
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -9,18 +7,18 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
+import { useContext } from 'react';
+import { AuthContext } from '../App';
 
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useContext(AuthContext);
 
-  function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    auth.signInWithPopup(provider).then(result => {
-      console.log(result);
-
-      history.push('/rooms/new');
-    })
+  async function handleCreateRoom() {
+    if(!user) {
+      await signInWithGoogle();
+    }
+    history.push('/rooms/new');
   }
 
   return (
